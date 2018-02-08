caa4a5b992c49d2ecd5fcb7ea0c4b9bf5d2d8c33
Davide Polonio <poloniodavide@gmail.com>
2018-02-08 16:53:45 +0100

    Add new file to not spell check
    # Conflicts:
    #	tools/spellcheck.sh

--- tools/spellcheck.sh
+++ tools/spellcheck.sh
@@ -2,12 +2,15 @@
 
 for i in $(git diff-tree --no-commit-id --name-only -r HEAD | grep .tex);
 do
-  echo "-- Checking file $i --"
-  tmp=$(cat $i | aspell --lang=en -t -a --add-extra-dicts="`pwd`/tools/dictionary.en.pws" | grep -v "*" | grep -v "@" | sed '/^\s*$/d')
-  echo "-- !!List of errors detected for $i!! --"
-  echo "$tmp"
-  echo "-- !!End of list for $i!! --"
-  res=$res$tmp
+    if [ "$i" != "config/config.tex" ] && [ "$i" != "config/package.tex" ] && [ "$i" != "main.tex" ]
+    then
+	echo "Checking file $i"
+	tmp=$(cat $i | aspell -t -a --add-extra-dicts="`pwd`/tools/dictionary.en.pws" | grep -v "*" | grep -v "@" | sed '/^\s*$/d')
+	echo "List of errors detected for $i"
+	echo "$tmp"
+	echo "End of list for $i"
+	res=$res$tmp
+    fi
 done
 
 if [ "$res" != "" ]
